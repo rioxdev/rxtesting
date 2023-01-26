6c3b43111354ac385a48afde5565cc25ea825bde
Henrio Randriasoa <henrio.randriasoa@viseo.com>
2023-01-26 14:23:59 +0100

    Refactor de CalculatorTests

--- rxtesting.tests/CalculatorTests.cs
+++ rxtesting.tests/CalculatorTests.cs
@@ -9,26 +9,26 @@ namespace rxtesting.tests
     public class CalculatorTests
     {
         private ITestOutputHelper OutputHelper { get; }
+        private Calculator _calc;
 
         public CalculatorTests(ITestOutputHelper outputHelper)
         {
+            //Arrange
+
             OutputHelper = outputHelper;
+            var services = new ServiceCollection()
+                .AddLogging((builder) => builder.AddXUnit(OutputHelper))
+                .AddScoped<Calculator>();
+
+            _calc = services.BuildServiceProvider()
+                .GetRequiredService<Calculator>();
         }
 
         [Fact]
         public void Add_GivenTwoInt_ReturnsInt()
         {
-            //Arrange
-            var services = new ServiceCollection()
-            .AddLogging((builder) => builder.AddXUnit(OutputHelper))
-            .AddScoped<Calculator>();
-
-            var calc = services
-                .BuildServiceProvider()
-                .GetRequiredService<Calculator>();
-
             //Act
-            var result = calc.Add(1, 3);
+            var result = _calc.Add(1, 3);
 
             //Assert
             Assert.Equal(4, result);
@@ -37,14 +37,7 @@ namespace rxtesting.tests
         [Fact]
         public void Add_GivenTwoDoubles_ReturnsDouble()
         {
-            var services = new ServiceCollection()
-                .AddLogging((builder) => builder.AddXUnit(OutputHelper))
-                .AddSingleton<Calculator>();
-
-            var calc = services
-                .BuildServiceProvider()
-                .GetRequiredService<Calculator>();
-            var result = calc.AddDouble(2.57, 5.21);
+            var result = _calc.AddDouble(2.57, 5.21);
             Assert.Equal(7.8, result, 1);
         }
 
@@ -54,15 +47,7 @@ namespace rxtesting.tests
 
         public void Add_TestIntValues(int a, int b, int expected)
         {
-            var services = new ServiceCollection()
-                .AddLogging((builder) => builder.AddXUnit(OutputHelper))
-                .AddSingleton<Calculator>();
-
-            var calc = services
-                .BuildServiceProvider()
-                .GetRequiredService<Calculator>();
-
-            var result = calc.Add(a, b);
+            var result = _calc.Add(a, b);
 
             result.Should().Be(expected);
         }
